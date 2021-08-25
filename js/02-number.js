138ca31f6d36ab2b76c5e7e9950d17b2d04a24c0
박상혁 <huhaha9142@naver.com>
2021-08-25 17:55:34 +0900

    basic, number.js 수정

--- js/02-number.js
+++ js/02-number.js
@@ -16,7 +16,8 @@ console.log("PI 상수:", Math.PI);
 console.log("최대값:", Math.max(1, 3, 5, 2, 6, 7));
 console.log("최소값:", Math.min(1, 3, 5, 2, 6, 7));
 //  Math.round : 반올림
-console.log(Math.round(3.567), Math.trunc(3.567), Math.floor(3.567));
+console.log(Math.round(3.567), Math.trunc(3.567), Math.floor(3.567));   
+// Math.trunc : 값의 소수부분을 제거하고 정수 부분만 표현
 
 //  그 외 Number 객체가 가진 상수들
 console.log("표현할 수 있는 최대 정수:", Number.MAX_SAFE_INTEGER);
@@ -28,8 +29,10 @@ console.log("-------------- Casting: 타입 변환");
 //  중간에 데이터 타입이 변경될 수 있다 -> 형변환이 필요한 경우 많다
 //  parse 계열: parseInt, parseFloat
 console.log("011", parseInt("011"));
+// console.log(typeof "011", typeof parseInt("011"));   --> "011" : String , parseInt("011") : number
 console.log("011", parseInt("011", 2)) //  2진수 011을 변환
 console.log("0xFF", parseInt("0xFF", 16))  //  16진수 FF를 변환
+// console.log(typeof "0xFF", typeof parseInt("0xFF"));  --> "0xFF" : String, parseInt("0xFF, 16") : number 16진수 변환
 console.log("123.456", parseInt("123.456"));    //  정수형으로 변환 -> 123
 console.log("123.456", parseFloat("123.456"));  //  실수형으로 변환 -> 123.456
 
@@ -37,8 +40,8 @@ console.log("123.456", parseFloat("123.456"));  //  실수형으로 변환 -> 12
 //  다른 타입으로 변경도 가능
 console.log("PI", parseInt(Math.PI));   //  float -> int로 변환
 
-//  중요: NaN - 숫자로 변환할 수 없는 문자열을 변환하면 NaN(Not A Number) 
-let v = parseInt("abc1234567");
+//  중요!!!: NaN - 숫자로 변환할 수 없는 문자열을 변환하면 NaN(Not A Number) 
+let v = parseInt("park1234567");
 console.log(v, "is ", typeof v);
 //  NaN가 포함된 산술 연산 -> NaN
 console.log(v + 2021);  //  NaN + 2021
@@ -54,4 +57,4 @@ console.log(v + 2021);  //  Infinity + 2021 -> Infinity
 //  유한수치인지 확인 isFinite 함수
 console.log("1/0 is Finite?", isFinite(1/0));
 console.log("2021 is Finite?", isFinite(2021));
-console.log("NaN is Finite", isNaN(parseInt("abc123")));
+console.log("NaN is Finite", isFinite(parseInt("abc123")));
