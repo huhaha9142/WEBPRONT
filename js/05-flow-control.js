52289aacea79e64f5564feb10650b570c84ad719
박상혁 <huhaha9142@naver.com>
2021-08-26 13:20:12 +0900

    향상된 for문

--- js/05-flow-control.js
+++ js/05-flow-control.js
@@ -37,3 +37,42 @@ console.log("-----연습문제 2-----");
 // for (i=1; i<=5; i++) {
 //     console.log("*****")
 // }
+
+
+
+
+//  ES6: for ... in
+//          객체의 속성(프로퍼티)를 순회
+//  ES6: for ... of
+//          순회 객체의 값을 순회
+let obj = {         // <--- 자바스크립트 객체 표기법
+    name: "박상혁",
+    age: 32,
+    job: "학생",
+    gender: "남성"
+}
+console.log("===== for ... in");
+
+for (let key in obj) {
+    //  for ... in : 객체의 속성을 순회
+    console.log(key, "->", obj[key]);
+}
+
+const arr = [5, 1, 4, 3, 2, 9, 8, 0];
+for (let key in arr) {
+    console.log(key); //    -> 0 1 2 3 4 5 6 7
+}
+    console.log();
+
+console.log("==== for ... of");
+for (let value of arr) {
+    //  for ... of : 반복 가능 객체에서 요소를 하나씩
+    console.log(value);
+}
+
+//  for ... of는 순회 객체(배열 등)에서만 사용 가능
+// for (let value of obj) {     
+//     console.log(value);
+// } --> Error: obj는 순회객체 아님
+
+// 주의!! : for ... in과 for ... of를 구분해서 사용
